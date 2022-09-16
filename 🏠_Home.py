d462ce9655265ab2d5e87335a49a8102ed5dce75
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-09-16 20:19:38 +0700

    feat: added homepage

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 
 def main():
-    st.header("Assignment Name")
-    st.subheader("Class Name")
+    st.header("Assignment 2: CWT")
+    st.subheader("Nonstationary Signal Analysis")
     st.markdown("Aditya Wardianto 07311940000001")
     st.markdown("Made with ❤️, 🦾,  and ☕")
+
 
 if __name__ == "__main__":
     main()
