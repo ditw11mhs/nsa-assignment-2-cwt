782c3efecf106ed7824c70c5f269d16b432c9339
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-09-19 12:15:07 +0700

    fix: increase message limit

--- pages/01_💻_Application.py
+++ pages/01_💻_Application.py
@@ -1,9 +1,31 @@
-from api import logic, utils
+import os
+
+import plotly.express as px
 import streamlit as st
+from plotly import graph_objects as go
+
+from api import logic, utils
 
 
 def main():
     st.header("App")
+
+    output = logic.data_loader(os.path.join("data", "pcg_1.txt"))
+    st.dataframe(output)
+
+    import numpy as np
+
+    inp = np.arange(-100, 100, 0.01)
+    w_re, w_im = logic.complex_morlet_wavelet(inp)
+    st.line_chart(w_re)
+
+    dummy = np.arange(100)
+    cwt = logic.compute_cwt(output, logic.complex_morlet_wavelet, 0.01, 100)
+    # fig = go.Figure(data=go.Heatmap(z=cwt, connectgaps=True, zsmooth="best"))
+    # fig = go.Figure(data=go.Contour(z=cwt))
+    # fig = px.imshow(cwt)
+    # st.plotly_chart(fig)
+    print(cwt)
 
 
 if __name__ == "__main__":
