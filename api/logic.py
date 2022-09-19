7be4cc111766d8570dce61ddf983915974fa3f34
Aditya Wardianto <83759122+ditw11mhs@users.noreply.github.com>
2022-09-19 15:30:49 +0700

    refactor: optimize wavelet matrix function

--- api/logic.py
+++ api/logic.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from numba import jit
+from numba import jit, njit
 from scipy import signal
 
 
@@ -9,20 +9,36 @@ def data_loader(file_path: str, col_name: str = ["Time", "Data"]):
     return output
 
 
-@jit
-def complex_morlet_wavelet(t):
-    f_o = 0.849
-    w_o = 2 * np.pi * f_o
+def get_wavelet_function(function_name: str):
+    wave_func_dict = {"Complex Morlet": complex_morlet_wavelet}
 
-    w_re = np.power(np.pi, (-1 / 4)) * np.exp((-(t**2)) / 2) * np.cos(w_o * t)
-    w_im = np.power(np.pi, (-1 / 4)) * np.exp((-(t**2)) / 2) * np.sin(w_o * t)
+    return wave_func_dict[function_name]
+
+
+@njit
+def complex_morlet_wavelet(t):
+    # f_o = 0.849
+    # w_o = 2 * np.pi * f_o
+    # power_pi = np.power(np.pi,(-0.25))
+    # Precomputation to further optimize calucaltion
+
+    w_o = 5.33442
+    power_pi = 0.75113
+    pre_compute = power_pi * np.exp((-np.square(t)) * 0.5)
+    w_re = pre_compute * np.cos(w_o * t)
+    w_im = pre_compute * np.sin(w_o * t)
     return w_re, w_im
 
 
-@jit
+@njit
 def compute_t_matrix(wavelet_function, t, scale):
     t_matrix = t / scale
     return wavelet_function(t_matrix)
+
+
+@jit(forceobj=True)
+def numba_correlate(input_1, input_2, mode):
+    return signal.correlate(input_1, input_2, mode)
 
 
 def compute_cwt(
@@ -41,15 +57,15 @@ def compute_cwt(
     scale = np.arange(scale_limit_down, scale_limit_up, scale_resolution).reshape(-1, 1)
     scale = np.flip(scale, axis=0)  # <- This makes an decreasing scale
     print("Preparation Done!")
-    
+
     # Create a 2d matrix of wavelet using 2d matrix of t as an input
     w_re_matrix, w_im_matrix = compute_t_matrix(wavelet_function, t, scale)
     print("Wavelet Matrix Done!")
-    
+
     # Calculate CWT using the correlation of data against each row of the wavelet matrix
-    cwt_re = signal.correlate(w_re_matrix, data, "valid")
+    cwt_re = numba_correlate(w_re_matrix, data, "valid")
     print("CWT Real Done!")
-    cwt_im = signal.correlate(w_im_matrix, data, "valid")
+    cwt_im = numba_correlate(w_im_matrix, data, "valid")
     print("CWT Imaginary Done!")
     cwt = np.sqrt(np.square(cwt_re) + np.square(cwt_im)) / np.sqrt(scale)
     print("Last CWT Done! \n")
